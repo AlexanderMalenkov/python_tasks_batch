d3d0460704398d72c1d759532687ea74d5181b26
AlexanderMalenkov <malenkov2001@gmail.com>
2020-10-10 22:39:39 +0300

    fix

--- 1_input_output/DivisionApples.py
+++ 1_input_output/DivisionApples.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
+"""
+User has to insert number of schoolboys and number of apples
+"""
 
-"""User has to insert number of schoolboys and number of apples"""
 
-#identify variables num_students and num_apples
-
+# Identify variables num_students and num_apples
 num_students = 0
 num_apples = 0
-
-#enter number of students and number of apples
-
+# Enter number of students and number of apples
 num_students = int(input('Enter number of students: '))
 num_apples = int(input('Enter number of apples: '))
-
-#cheking for correctness and find and output result of division
-
+# Cheking for correctness and find and output result of division
 if num_students and num_apples > 0:
     print('Each of the students will recieve: ', num_apples // num_students)
     print('Will remain:', num_apples % num_students)

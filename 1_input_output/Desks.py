effd3585621b50e48eba5ea71eeadff2f67bb233
AlexanderMalenkov <malenkov2001@gmail.com>
2020-10-10 22:37:56 +0300

    fix

--- 1_input_output/Desks.py
+++ 1_input_output/Desks.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
+"""
+Program calculates number of desks for schoolboys from 3 classes
+"""
 
-"""Program calculates number of desks for schoolboys from 3 classes"""
 
-#identify variables for all classes
-
+# Identify variables for all classes
 class1 = 0
 class2 = 0
 class3 = 0
-
-#entering number of schoolboys
-
+# Entering number of schoolboys
 class1 = int(input('Enter number of students in the first class: '))
 class2 = int(input('Enter number of students in the second class: '))
 class3 = int(input('Enter number of students in the third class: '))
-
-#checking for correctness and finding result
-
+# Checking for correctness and finding result
 if class1 and class2 and class3 > 0:
     if (class1 + class2 + class3) % 2 == 0:
-        print ('You need', (class1 + class2 + class3) // 2, 'desks')
+        print('You need', (class1 + class2 + class3) // 2, 'desks')
     else:
-        print ('You need', ((class1 + class2 + class3) // 2)+1,'desks')
+        print('You need', ((class1 + class2 + class3) // 2)+1,'desks')
 else:
     print('Uncorrectly data.Try again')
 

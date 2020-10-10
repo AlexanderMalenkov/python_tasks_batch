29e035a95770580db0353bfc10c35ba65241abe8
AlexanderMalenkov <malenkov2001@gmail.com>
2020-10-10 22:43:31 +0300

    fix

--- 1_input_output/DigitalClock.py
+++ 1_input_output/DigitalClock.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+Programm calculates the number of hours in a given number of minutes
+"""
 
-"""Programm calculates the number of hours in a given number of minutes"""
 
-#enter number of minutes and identify final hours and minutes
-
+# Enter number of minutes and identify final hours and minutes
 hours = 0
 minutes = 0
 num_min = int(input('Enter number of minutes: '))
-
-#calculating the number of minutes and hours in the entered minutes and checking for correctness and cheking for the number of days in the entered minutes
-
+# Calculating the number of minutes and hours in the entered minutes and checking for correctness and cheking for the number of days in the entered minutes
 if num_min > 0:
      hours = num_min // 60
      minutes = num_min % 60
      if num_min < 1440:
-         print (hours, 'H', minutes, 'Min')
+         print(hours, 'H', minutes, 'Min')
      else:
-         print (hours % 24, 'H', minutes, 'Min')
+         print(hours % 24, 'H', minutes, 'Min')
 else:
-    print ('Uncorrectly data. Try again')
+    print('Uncorrectly data. Try again')
 
 

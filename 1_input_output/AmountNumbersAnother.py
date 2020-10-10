98d4f3f66e234448ba169148a9de542af8518d0f
AlexanderMalenkov <malenkov2001@gmail.com>
2020-10-10 22:36:58 +0300

    fix

--- 1_input_output/AmountNumbersAnother.py
+++ 1_input_output/AmountNumbersAnother.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-
 """
 User has to enter three numbers and programm will add them up
 """
-# identify a variable Sum
 
+
+# Identify a variable Sum
 sum = 0
-
-# add up numbers in the cycle
-
+# Add up numbers in the cycle
 for i in range(3):
     sum += int(input())
-
-# output the result
-
+# Output the result
 print(sum)

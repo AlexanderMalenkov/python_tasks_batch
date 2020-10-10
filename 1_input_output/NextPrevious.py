5a3f2d545ccb8f9de2e9aa47adde06322002f1ce
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-10 17:56:52 +0300

    Add files via upload

---
+++ 1_input_output/NextPrevious.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Program calculate next and previous number"""
+
+#entering number
+
+num = int(input('Enter your number: ',))
+
+#finding next and previous number and output the result
+
+print('The next number for the number', num, 'is', num + 1, '.', 'The previous number for the number', num, 'is', num - 1,'.')
+
+
+

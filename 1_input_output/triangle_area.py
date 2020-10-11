b7e62af5a02977f992f0b1b99c8316c1eb2aa67f
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-11 13:06:12 +0300

    fix

--- 1_input_output/triangle_area.py
+++ 1_input_output/triangle_area.py
@@ -5,12 +5,12 @@ User has to enter length of triangle legs and program will substitute input data
 
 
 # Input lenth
-leg1 = int(input('Enter lenth of the first leg: '))
-leg2 = int(input('Enter lenth of the second leg: '))
+first_leg = int(input('Enter lenth of the first leg: '))
+second_leg = int(input('Enter lenth of the second leg: '))
+
 # Cheking for correctness and substitute input data in formula and output result
-if leg1 > 0 and leg2 > 0:
-    area = (leg1 * leg2)/2
-    print('Area of triangle: ', area)
+if first_leg > 0 and second_leg > 0:
+    print('Area of triangle: ', (first_leg * second_leg) / 2)
 else:
     print('Uncorrectly data.Try again')
 

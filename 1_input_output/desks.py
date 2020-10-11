f2874957692130470ab534b12a1153921a77c51f
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-11 12:55:45 +0300

    fix

--- 1_input_output/desks.py
+++ 1_input_output/desks.py
@@ -8,17 +8,18 @@ Program calculates number of desks for schoolboys from 3 classes
 class1 = 0
 class2 = 0
 class3 = 0
+
 # Entering number of schoolboys
 class1 = int(input('Enter number of students in the first class: '))
 class2 = int(input('Enter number of students in the second class: '))
 class3 = int(input('Enter number of students in the third class: '))
+
 # Checking for correctness and finding result
-if class1 and class2 and class3 > 0:
+if class1 > 0 and  class2 > 0 and class3 > 0:
     if (class1 + class2 + class3) % 2 == 0:
         print('You need', (class1 + class2 + class3) // 2, 'desks')
     else:
         print('You need', ((class1 + class2 + class3) // 2)+1,'desks')
 else:
     print('Uncorrectly data.Try again')
-
-
+    

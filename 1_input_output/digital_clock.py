de5bbe38d4852b488840a5edd356a2779e11c710
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-11 12:58:33 +0300

    fix

--- 1_input_output/digital_clock.py
+++ 1_input_output/digital_clock.py
@@ -8,6 +8,7 @@ Programm calculates the number of hours in a given number of minutes
 hours = 0
 minutes = 0
 num_min = int(input('Enter number of minutes: '))
+
 # Calculating the number of minutes and hours in the entered minutes and checking for correctness and cheking for the number of days in the entered minutes
 if num_min > 0:
      hours = num_min // 60

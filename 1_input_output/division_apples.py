6679d121bd6cfa29b64869d7f3318168adef6b08
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-11 12:59:33 +0300

    fix

--- 1_input_output/division_apples.py
+++ 1_input_output/division_apples.py
@@ -7,11 +7,13 @@ User has to insert number of schoolboys and number of apples
 # Identify variables num_students and num_apples
 num_students = 0
 num_apples = 0
+
 # Enter number of students and number of apples
 num_students = int(input('Enter number of students: '))
 num_apples = int(input('Enter number of apples: '))
+
 # Cheking for correctness and find and output result of division
-if num_students and num_apples > 0:
+if num_students > 0 and num_apples > 0:
     print('Each of the students will recieve: ', num_apples // num_students)
     print('Will remain:', num_apples % num_students)
 else:

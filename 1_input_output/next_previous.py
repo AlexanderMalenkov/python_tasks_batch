018781d2cb7dad018fa64f5bf0b05b773ed082f8
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-11 13:02:24 +0300

    fix

--- 1_input_output/next_previous.py
+++ 1_input_output/next_previous.py
@@ -6,6 +6,7 @@ Program calculate next and previous number
 
 # Entering number
 num = int(input('Enter your number: ',))
+
 # Finding next and previous number and output the result
 print (f'The next number of the number {num} is {num + 1}')
 print (f'The previous number of the number {num} is {num - 1}')

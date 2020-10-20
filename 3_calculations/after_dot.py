0f403111e4fcb603e31f0ed9f213376e9997447c
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-20 17:59:19 +0300

    fix

--- 3_calculations/after_dot.py
+++ 3_calculations/after_dot.py
@@ -7,3 +7,4 @@ The program finds the fractional part of the number(first symbol after the dot)
 
 # Entering data and finding the result
 print('The first symbol after dot is', abs(int(float(input('Enter your number: '))* 10) % 10))
+

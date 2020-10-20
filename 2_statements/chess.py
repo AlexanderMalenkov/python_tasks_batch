6e08d308987764785c576feb9252e96b4ea0ce91
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-20 11:51:39 +0300

    fix

--- 2_statements/chess.py
+++ 2_statements/chess.py
@@ -5,30 +5,19 @@ Program determines whether the chessboard cells are the same color
 """
 
 
+# Entering an array for coordinates and variables
+cells = [] 
+coord = 0
 
-# Entering cells' coordinates
-cell_x1 = int(input('Enter X: '))
-cell_y1 = int(input('Enter Y: '))
-cell_x2 = int(input('Enter X: '))
-cell_y2 = int(input('Enter Y: '))
+# Enter a number and check it for belonging to the interval
+for i in range(4):
+    coord = int(input('Enter your coordinates: ')) 
+    if coord not in range(1,9):
+          raise Exception('Incorrect data')
+    cells.append(coord)
 
-# Checking for correctness and checking for a neibor's location and ouput the result
-if cell_x1 in range(1,9) and cell_x1 in range(1,9):
-    if cell_y1 in range(1,9) and cell_y1 in range(1,9):
-        if cell_x2 in range(1,9) and cell_x2 in range(1,9):
-            if cell_y2 in range(1,9) and cell_y2 in range(1,9):
-                if (cell_x1 + cell_x2 + cell_y1 + cell_y2) % 2 == 0:
-                    print('Yes')
-                else:
-                    print('No')
-
-            else:
-                print('Uncorrectly data. Try again')
-        else:
-             print('Uncorrectly data. Try again')
-    else:
-        print('Uncorrectly data. Try again')
+# Finding and output result
+if sum(cells) % 2 == 0:
+    print('Yes')
 else:
-    print('Uncorrectly data. Try again')
-
-
+    print('No')

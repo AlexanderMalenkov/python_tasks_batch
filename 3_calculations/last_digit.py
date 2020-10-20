0a765077951efd9c4f276653f1b4aea657df0641
Alexander Malenkov <53808585+AlexanderMalenkov@users.noreply.github.com>
2020-10-20 18:27:03 +0300

    fix

--- 3_calculations/last_digit.py
+++ 3_calculations/last_digit.py
@@ -5,10 +5,12 @@ The program finds the last digit of the number
 """
 
 
-# Identify a variable
+# Identify a variable and checking for correctness
 num = int(input('Enter your number: '))
-
+if insistance(num, str):
+  raise Exception('Your entered the letter')
+  
 # Finding result and output it
-print(f'Last digit of the number {num} is {num % 10}')
+print(f'Last digit of the number {num} is {abs(num % 10)}')
 
 
